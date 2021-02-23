c09eea1f87c88c9d87fa7d833aea066c8c61e101
Ton-10 <taveon30@yahoo.com>
2021-02-23 17:08:38 -0600

    Add combat loop and engagement scripts/logic

---
+++ Assets/Scripts/CombatManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    public List<GameObject> Combatants = new List<GameObject>();
+    public List<GameObject> Players = new List<GameObject>();
+    public List<GameObject> Enemies = new List<GameObject>();
+    public GameObject CurrentActor;
+    private int currentIndice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentIndice = 0;
+        foreach (GameObject combatant in Combatants)
+        {
+            EntityStats entityStats = combatant.GetComponent<EntityStats>();
+            entityStats.CanAttack = false;
+            entityStats.TurnFinished = false;
+            if (combatant.CompareTag("Player"))
+            {
+                combatant.GetComponent<OVRPlayerController>().EnableLinearMovement = entityStats.CanAttack;
+            }
+        }
+
+        SplitCombatantTypes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool areEnemiesAlive = Enemies.Select(enemy => enemy.GetComponent<EntityStats>().HP > 0).ToList().Contains(true);
+        bool arePlayersAlive = Players.Select(player => player.GetComponent<EntityStats>().HP > 0).ToList().Contains(true);
+
+        bool isFightOver = !areEnemiesAlive || !arePlayersAlive;
+
+        // Core fight loop logic
+        if (!isFightOver)
+        {
+            int numberOfCombatants = Combatants.Count;
+            CurrentActor = Combatants[currentIndice];
+            EntityStats currentActorStats = CurrentActor.GetComponent<EntityStats>();
+
+            currentActorStats.CanAttack = true;
+            if (CurrentActor.CompareTag("Player"))
+            {
+                CurrentActor.GetComponent<OVRPlayerController>().EnableLinearMovement = currentActorStats.CanAttack;
+            }
+            
+
+            if (CurrentActor.GetComponent<EntityStats>().TurnFinished)
+            {
+                currentActorStats.TurnFinished = false;
+                currentActorStats.CanAttack = false;
+
+                if (CurrentActor.CompareTag("Player"))
+                {
+                    CurrentActor.GetComponent<OVRPlayerController>().EnableLinearMovement = currentActorStats.CanAttack;
+                }
+
+                currentIndice++;
+
+                if (currentIndice > numberOfCombatants - 1)
+                {
+                    currentIndice = 0;
+                }
+            }
+
+        }
+        else
+        {
+            Debug.Log("Fight Over");
+            Destroy(this);
+        }
+
+    }
+
+    public void SplitCombatantTypes()
+    {
+        Players = new List<GameObject>();
+        Enemies = new List<GameObject>();
+      foreach (GameObject combatant in Combatants)
+        {
+            if (combatant.CompareTag("Player"))
+            {
+                Players.Add(combatant);
+            }
+            else
+            {
+                Enemies.Add(combatant);
+            }
+        }
+    }
+
+    public void AddPlayer(GameObject player)
+    {
+        Players.Add(player);
+    }
+
+    public void AddCombatant(GameObject combatant)
+    {
+        Combatants.Add(combatant);
+    }
+}
---
+++ Assets/Scripts/EncounterListenerScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EncounterListenerScript : MonoBehaviour
+{
+    public List<Collider> HitColliders;
+    public List<GameObject> Combatants;
+    public CombatManager CombatManager;
+    public int Enemies, Players;
+    public bool StartFight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Combatants = new List<GameObject>();
+    }
+
+    // for visually debugging distance
+    private void OnDrawGizmosSelected()
+    {
+     Gizmos.color = Color.red;
+     //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
+     Gizmos.DrawWireSphere(gameObject.transform.position, 8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get all gameBbjects within radius from player
+        HitColliders = Physics.OverlapSphere(gameObject.transform.position, 8).ToList();
+
+        int currentNumberEnemies = Enemies;
+
+        // Add the found players and enemies to the combatants list
+
+        List<GameObject> playerList = HitColliders
+            .Select(obj => obj.gameObject)
+            .Where(obj => obj.CompareTag("Player"))
+            .ToList();
+
+        List<GameObject> enemyList = HitColliders
+            .Select(obj => obj.gameObject)
+            .Where(obj => obj.CompareTag("Enemy"))
+            .ToList();
+
+        Players = playerList.Count;
+        Enemies = enemyList.Count;
+
+        Combatants = playerList
+            .Concat(enemyList)
+            .OrderByDescending(obj => obj.GetComponent<EntityStats>().SPEED)
+            .ToList();
+
+        if(currentNumberEnemies < Enemies)
+        {
+
+        }
+
+        // Initiate fight if there are Combatants and the StartFight bool is on.
+        if (Combatants != null && StartFight == true)
+        {
+            StartFight = false;
+            foreach (var combatant in Combatants)
+            {
+                if (combatant.GetComponent<CombatManager>())
+                {
+                    CombatManager = combatant.GetComponent<CombatManager>();
+                    List<GameObject> newCombatants = Combatants.Except(CombatManager.Combatants).ToList();
+                    foreach (var item in newCombatants)
+                    {
+                        CombatManager.AddCombatant(item);
+                    }
+                    CombatManager.SplitCombatantTypes();
+                }
+            }
+            if(CombatManager == null)
+            {
+                CombatManager = gameObject.AddComponent<CombatManager>();
+                CombatManager.Combatants = Combatants;
+            }
+        }
+    }
+}
